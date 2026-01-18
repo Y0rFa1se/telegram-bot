89be1766258db5ca428cfffd7d9cb96325c5c03a
Y0rFa1se <neargul123@gmail.com>
2026-01-19 03:42:50 +0900

    sample

--- modules/pupil/pupil.py
+++ modules/pupil/pupil.py
@@ -22,7 +22,7 @@ class PupilHandler(Handler):
         pupil_list = [pupil["text"] for pupil in self.pupils[self.cursor : self.cursor + 10]]
         
         await BotAPI.reply(
-            update, context, "\n".join(pupil_list), reply_markup=reply_markup
+            update, context, "hl\n\n" + "\n".join(pupil_list), reply_markup=reply_markup
         )
 
     @command("ha", "pupil add")
@@ -54,12 +54,16 @@ class PupilHandler(Handler):
 
     @command("hr", "pupil random")
     async def hr_cmd(self, update, context):
+        keyboard = [
+            [("sample", "pupil_hr_sample")]
+        ]
+        reply_markup = Utils.inline_keyboard(keyboard)
         args = await BotAPI.get_args(update, context)
         if args and args[0].isdigit():
             n = int(args[0])
-            await BotAPI.reply(update, context, "\n".join(self.pupils % n))
+            await BotAPI.reply(update, context, "\n".join(self.pupils % n), reply_markup=reply_markup)
             return
-        await BotAPI.reply(update, context, (self.pupils % 1)[0])
+        await BotAPI.reply(update, context, (self.pupils % 1)[0], reply_markup=reply_markup)
         
     @command("hs", "pupil search")
     async def hs_cmd(self, update, context):
@@ -70,7 +74,7 @@ class PupilHandler(Handler):
             return
         
         res = self.pupils @ arg
-        await BotAPI.reply(update, context, "\n".join(res))
+        await BotAPI.reply(update, context, "hs\n\n" + "\n".join(res))
         
     @command("hstart", "pupil startswith")
     async def hstart_cmd(self, update, context):
@@ -81,7 +85,7 @@ class PupilHandler(Handler):
             return
         
         res = self.pupils ^ arg
-        await BotAPI.reply(update, context, "\n".join(res))
+        await BotAPI.reply(update, context, "hstart\n\n" + "\n".join(res))
         
     @command("hend", "pupil endswith")
     async def hend_cmd(self, update, context):
@@ -92,7 +96,7 @@ class PupilHandler(Handler):
             return
         
         res = self.pupils / arg
-        await BotAPI.reply(update, context, "\n".join(res))
+        await BotAPI.reply(update, context, "hend\n\n" + "\n".join(res))
         
     @callback("pupil_hl_next")
     async def pupil_hl_next_callback(self, update, context):
@@ -131,5 +135,19 @@ class PupilHandler(Handler):
         await CallbackAPI.edit(
             query,
             text="\n".join(pupil_list) if pupil_list else "No more pupils.",
+            reply_markup=reply_markup,
+        )
+        
+    @callback("pupil_hr_sample")
+    async def pupil_hr_sample_callback(self, update, context):
+        keyboard = [
+            [("sample", "pupil_hr_sample")]
+        ]
+        reply_markup = Utils.inline_keyboard(keyboard)
+        query = await CallbackAPI.get_callback(update, context)
+        sample = (self.pupils % 1)[0]
+        await CallbackAPI.edit(
+            query,
+            text=sample,
             reply_markup=reply_markup,
         )
